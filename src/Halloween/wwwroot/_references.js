117f9880ca3909faa28d1678d767a37f7c5e138f
Bill Boga <bill.boga@outlook.com>
2015-10-06 00:04:00 -0400

    Add Signalr package

--- src/Halloween/wwwroot/_references.js
+++ src/Halloween/wwwroot/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/displays/text-glitch-display.js" />
+/// <reference path="lib/jquery.signalr/jquery.signalr.js" />
 /// <reference path="lib/jquery/jquery.js" />
