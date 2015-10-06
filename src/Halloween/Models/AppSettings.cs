72e85a764892319f5ac492504b8e6ed9c26e9ea5
Bill Boga <bill.boga@outlook.com>
2015-10-06 00:05:27 -0400

    Add AppSetting denoting portrait mode for displays

--- src/Halloween/Models/AppSettings.cs
+++ src/Halloween/Models/AppSettings.cs
@@ -7,5 +7,6 @@ namespace Halloween.Models
         public string DisplayHeight { get; set; }
         public string DisplayWidth { get; set; }
         public Dictionary<string, Dictionary<string, string>> Displays { get; set; }
+        public bool PortraitMode { get; set; }
     }
 }
