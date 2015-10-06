250f5aec414f4e1a162a687863910ad8dbcf3125
Bill Boga <bill.boga@outlook.com>
2015-10-06 00:09:24 -0400

    Add preliminary pin provider and debug implementation
    This feature will allow setup of IO pins and triggering notification (intended usage is with Raspberry Pi).
    For now, a debug implementation will help during quick iteration development.

---
+++ src/Halloween/Models/DebugPinProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Halloween.Models
+{
+    public class DebugPinProvider : IPinProvider
+    {
+        public DebugPinProvider()
+        {
+            OutputPinStateChange = (pinIndex, state) =>
+            {
+                Debug.WriteLine(
+                    $"Output pin [{pinIndex}] has changed state to {state.ToString().ToLower()}.");
+            };
+        }
+
+        public Action<int, bool> InputPinStateChange { get; set; }
+        public Action<int, bool> OutputPinStateChange { get; set; }
+
+        public void SetOutputPinState(int pinIndex, bool state)
+        {
+            OutputPinStateChange(pinIndex, state);
+        }
+    }
+}
---
+++ src/Halloween/Models/IPinProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Halloween.Models
+{
+    public interface IPinProvider
+    {
+        Action<int, bool> InputPinStateChange { get; set; }
+        Action<int, bool> OutputPinStateChange { get; set; }
+
+        void SetOutputPinState(int pinIndex, bool state);
+    }
+}
