0318a23eb73e17a55e23cbcd7876e089267bb181
Bill Boga <bill.boga@outlook.com>
2015-10-08 23:27:09 -0400

    Add cheap way to get random audio source from existing app. settings

---
+++ src/Halloween/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Halloween.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        private static Random randomizer = new Random((int)DateTimeOffset.UtcNow.UtcTicks);
+
+        public static T GetRandomElement<T>(this IEnumerable<T> value)
+        {
+            return value.ElementAt(randomizer.Next(value.Count()));
+        }
+    }
+}
